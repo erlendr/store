7455cb76a78356c6ff173d0d93c458e94715babc
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-11-04 22:11:04 +0100

    Added Download function

--- store.go
+++ store.go
@@ -1,43 +1,59 @@
 package store
 
 import (
-  "fmt"
-  "github.com/mitchellh/goamz/aws"
-  "github.com/mitchellh/goamz/s3"
-  "io/ioutil"
-  "mime"
+	"fmt"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
+	"io/ioutil"
+	"mime"
 )
 
-
 func CreateBucket() *s3.Bucket {
-  auth, err := aws.EnvAuth()
-  if err != nil {
-    println("store -- error connecting to AWS\n")
-    panic(err)
-  }
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		println("store -- error connecting to AWS\n")
+		panic(err)
+	}
 
-  fmt.Printf("store -- connected to AWS\n")
+	fmt.Printf("store -- connected to AWS\n")
 
-  S3 := s3.New(auth, aws.EUWest)
-  bucket := S3.Bucket("go-uploader")
-  println("store -- Bucket name: " + bucket.Name)
-  return bucket
+	S3 := s3.New(auth, aws.EUWest)
+	bucket := S3.Bucket("go-uploader")
+	println("store -- Bucket name: " + bucket.Name)
+	return bucket
 }
 
 func Upload(filename string) {
-  println("store -- Upload called with filename " + filename)
-  bucket := CreateBucket()
+	println("store -- Upload called with filename " + filename)
+	bucket := CreateBucket()
 
-  tempDir := "temp/"
-  data, err := ioutil.ReadFile(tempDir + filename)
-  if err != nil {
-    println("store -- error reading file '" + tempDir + filename + "'")
-  }
+	tempDir := "temp/"
+	data, err := ioutil.ReadFile(tempDir + filename)
+	if err != nil {
+		println("store -- error reading file '" + tempDir + filename + "'")
+		panic(err)
+	}
 
-  extension := filename[(len(filename)-4):]
+	extension := filename[(len(filename) - 4):]
 
-  err = bucket.Put(filename, data, mime.TypeByExtension(extension), s3.PublicRead)
-  if err != nil {
+	err = bucket.Put(filename, data, mime.TypeByExtension(extension), s3.PublicRead)
+	if err != nil {
+		println("store -- error during bucket PUT")
+		panic(err)
+	}
+}
+
+func Download (filename string) *ReadCloser {
+	println("store -- Download called with filename " + filename)
+	bucket := CreateBucket()
+
+	tempDir := "temp/"
+
+	readCloser, err = bucket.GetReader(filename)
+  if err := nil {
+    println("store -- error during bucket GET")
     panic(err)
   }
+
+  return *readCloser
 }
