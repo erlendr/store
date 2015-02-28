653ecfde648691b3e40d1ddcbfa875a7f860fd4e
Erlend Røsjø <erlendr@knowit.no>
2015-02-28 13:02:14 +0100

    Read AWS region and bucket name from env.

--- store.go
+++ store.go
@@ -1,72 +1,92 @@
 package store
 
 import (
-	"fmt"
-	"github.com/mitchellh/goamz/aws"
-	"github.com/mitchellh/goamz/s3"
-	"io"
-	"io/ioutil"
-	"mime"
+"fmt"
+"github.com/mitchellh/goamz/aws"
+"github.com/mitchellh/goamz/s3"
+"io"
+"io/ioutil"
+"mime"
+"os"
 )
 
 func CreateBucket() *s3.Bucket {
-	auth, err := aws.EnvAuth()
-	if err != nil {
-		println("store -- error connecting to AWS\n")
-		panic(err)
-	}
+  auth, err := aws.EnvAuth()
+  if err != nil {
+    println("store -- error connecting to AWS\n")
+    panic(err)
+  }
 
-	fmt.Printf("store -- connected to AWS\n")
+  fmt.Printf("store -- connected to AWS\n")
 
-	S3 := s3.New(auth, aws.EUWest)
-	bucket := S3.Bucket("go-uploader")
-	println("store -- Bucket name: " + bucket.Name)
-	return bucket
+  awsRegion := os.Getenv("STORE_AWSREGION")
+  if awsRegion == "" {
+    println("store -- error: environment variable STORE_AWSREGION not set")
+    panic("store -- STORE_AWSREGION not set")
+  }
+
+  region := aws.Regions[awsRegion]
+  if region.Name == "" {
+   panic("store -- region '" + awsRegion + "' is invalid") 
+  }
+  println("store -- AWS region: " + region.Name)
+
+  S3 := s3.New(auth, aws.Regions["eu-west-1"])
+  
+  bucketName := os.Getenv("STORE_BUCKETNAME")
+  if bucketName == "" {
+    println("store -- error: environment variable STORE_BUCKETNAME not set")
+    panic("store -- STORE_BUCKETNAME not set")
+  }
+  
+  bucket := S3.Bucket(bucketName)
+  println("store -- Bucket name: " + bucket.Name)
+  return bucket
 }
 
 func Upload(filename string) {
-	println("store -- Upload called with filename " + filename)
-	bucket := CreateBucket()
+  println("store -- Upload called with filename " + filename)
+  bucket := CreateBucket()
 
-	tempDir := "temp/"
-	data, err := ioutil.ReadFile(tempDir + filename)
-	if err != nil {
-		println("store -- error reading file '" + tempDir + filename + "'")
-		panic(err)
-	}
+  tempDir := "temp/"
+  data, err := ioutil.ReadFile(tempDir + filename)
+  if err != nil {
+    println("store -- error reading file '" + tempDir + filename + "'")
+    panic(err)
+  }
 
-	extension := filename[(len(filename) - 4):]
+  extension := filename[(len(filename) - 4):]
 
-	err = bucket.Put(filename, data, mime.TypeByExtension(extension), s3.PublicRead)
-	if err != nil {
-		println("store -- error during bucket PUT")
-		panic(err)
-	}
+  err = bucket.Put(filename, data, mime.TypeByExtension(extension), s3.PublicRead)
+  if err != nil {
+    println("store -- error during bucket PUT")
+    panic(err)
+  }
 }
 
 func UploadReader(filename string, data io.Reader, dataLength int64) {
-	println("store -- UploadReader called with filename " + filename)
+  println("store -- UploadReader called with filename " + filename)
 
-	bucket := CreateBucket()
+  bucket := CreateBucket()
 
-	extension := filename[(len(filename) - 4):]
-	err := bucket.PutReader(filename, data, dataLength, mime.TypeByExtension(extension), s3.PublicRead)
-	if err != nil {
-		println("store -- error during bucket PUT using PutReader")
-		panic(err)
-	}
+  extension := filename[(len(filename) - 4):]
+  err := bucket.PutReader(filename, data, dataLength, mime.TypeByExtension(extension), s3.PublicRead)
+  if err != nil {
+    println("store -- error during bucket PUT using PutReader")
+    panic(err)
+  }
 }
 
 func Download(filename string) io.ReadCloser {
-	println("store -- Download called with filename " + filename)
-	bucket := CreateBucket()
+  println("store -- Download called with filename " + filename)
+  bucket := CreateBucket()
 
-	rc, err := bucket.GetReader(filename)
+  rc, err := bucket.GetReader(filename)
 
-	if err != nil {
-		println("store -- error during bucket GET")
-		panic(err)
-	}
+  if err != nil {
+    println("store -- error during bucket GET")
+    panic(err)
+  }
 
-	return rc
+  return rc
 }
